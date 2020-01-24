f213ec1fc195b8d1103c15b03263872af07baff9
Aurian <durand.aurian@me.com>
2020-01-24 09:06:52 +0100

    READ JSON

--- moduleChargementDonnees.py
+++ moduleChargementDonnees.py
@@ -1,6 +1,21 @@
+import json
+
 if __name__ == "__main__":
     print('ok moduleChargementDonnees.py')
 
 def main():
     dataRepPath = 'entrer le chemin des données ici'
     print('\nmoduleChargementDonnees is ready')
+    readJson()
+
+def readJson():
+
+    with open('data.json') as jsonfile:
+        data = json.load(jsonfile)
+
+    for site in data['sites']:
+        print(site['nomSite'])
+        for tempFiles in site['temp']:
+            print(tempFiles)
+        for consoFiles in site['conso']:
+            print(consoFiles)
