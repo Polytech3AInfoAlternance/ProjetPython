f37597414883d1ed5276fc2bf59ae6e25b8e9494
Michaël <40799412+Olxe@users.noreply.github.com>
2020-01-24 10:42:59 +0100

    ReadCSV + ReadJSON

--- moduleChargementDonnees.py
+++ moduleChargementDonnees.py
@@ -7,18 +7,25 @@ class ChargementManager:
     def GetCSV(self, CSVPath):
         print(CSVPath)
 
-    def ReadCSV(self, CSVPAth):
-        print("CSV")
+    def ReadCSV(self, CSVPAth, CSVColumn, CSVType):
+        print(CSVPAth + ' -> ' + CSVColumn + ' -> ' + CSVType)
 
     def ReadJSON(self, JSONPath):
-        parse.readJson(JSONPath)
-        print("JSON")
+        data = parse.readJson(JSONPath)
+
+        # for each site, read all temp and conso files
+        for site in data['sites']:
+            # for each temp file, call the csv to data frame function
+            for tempFiles in site['temp']:
+                self.ReadCSV('jeu_de_donnees/' + site['nomSite'] + tempFiles, str(data['fieldTemp']), 'temp')
+
+            # for each conso file, call the csv to data frame function
+            for consoFiles in site['conso']:
+                self.ReadCSV('jeu_de_donnees/' + site['nomSite'] + consoFiles, str(data['fieldConso']), 'conso')
 
 def main():
     dataRepPath = 'entrer le chemin des données ici'
     print('\nmoduleChargementDonnees is ready')
-
-
 
     instance = ChargementManager()
     instance.ReadJSON('data.json')
