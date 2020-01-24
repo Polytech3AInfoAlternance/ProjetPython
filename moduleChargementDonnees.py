d55444293208796fbdce3a00eca00ceaac2d53bd
Michaël <40799412+Olxe@users.noreply.github.com>
2020-01-24 09:29:00 +0100

    Up

--- moduleChargementDonnees.py
+++ moduleChargementDonnees.py
@@ -3,8 +3,22 @@ import moduleChargementDonneesPackage.parseConfigJson as parse
 if __name__ == "__main__":
     print('ok moduleChargementDonnees.py')
 
+class ChargementManager:
+    def GetCSV(self, CSVPath):
+        print(CSVPath)
+
+    def ReadCSV(self, CSVPAth):
+        print("CSV")
+
+    def ReadJSON(self, JSONPath):
+        parse.readJson('data.json')
+        print("JSON")
+
 def main():
     dataRepPath = 'entrer le chemin des données ici'
     print('\nmoduleChargementDonnees is ready')
-    parse.readJson()
 
+
+
+    instance = ChargementManager()
+    instance.GetCSV("hello");
