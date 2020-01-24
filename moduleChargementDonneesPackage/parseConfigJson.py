58d76f9cd87c23dd93e3f0d674526448b8a4fa33
Aurian <durand.aurian@me.com>
2020-01-24 09:49:11 +0100

    push pour michael

--- moduleChargementDonneesPackage/parseConfigJson.py
+++ moduleChargementDonneesPackage/parseConfigJson.py
@@ -1,13 +1,20 @@
 import json
 
-def readJson():
+def readJson(path):
 
-    with open('data.json') as jsonfile:
+    with open(path) as jsonfile:
         data = json.load(jsonfile)
 
+    return data
+
+    # for each site, read all temp and conso files
     for site in data['sites']:
-        print(site['nomSite'])
+        print('site: ' + site['nomSite'])
+
+        # for each temp file, call the csv to data frame function
         for tempFiles in site['temp']:
-            print(tempFiles)
+            print('  temp file path: ' + tempFiles + ' ; field: ' + str(data['fieldTemp']))
+
+        # for each conso file, call the csv to data frame function
         for consoFiles in site['conso']:
-            print(consoFiles)
+            print('  conso file path: ' + consoFiles + ' ; field: ' + str(data['fieldConso']))
