4813328e1683f1dd964a67991afa2386c9fdbda7
Epulapp <Epulapp@epulapp1915.univ-lyon1.fr>
2020-01-28 14:03:44 +0100

    Add plot analysis

--- moduleAnalyseDonneesBrutes.py
+++ moduleAnalyseDonneesBrutes.py
@@ -2,29 +2,49 @@ if __name__ == "__main__":
     print('ok moduleAnalyseDonneesBrutes')
 
 import pandas
-import numpy as np
 import matplotlib.pyplot as plt
 
-pandas.options.display.max_rows = 10
+# pandas.options.display.max_rows = 10
 
 print(pandas.__version__)
 
-df = pandas.read_csv("jeu_donnees.txt", sep ="\t", header = 0, usecols = ["date","temp_1","temp_2","tot_a","puissance_a"])
+datas = pandas.read_csv("jeu_donnees.txt", sep="\t", header=0, usecols=["date", "temp_1", "temp_2"])
 
-print(type(df))
-df.head()
-print(df.shape)
-print(df.dtypes)
+run = True
 
-print(df[['date','temp_1','temp_2','tot_a']])
+while (run):
+    pas = input(
+        "Selection du pas:\n [1] Une semaine (7 jours)\n [2] Une journée\n [3] 12h\n [4] 6h\n [5] 1h\n Votre choix: ")
+    if (pas == "1"):
+        pas = 1008
+        run = False
+    elif (pas == "2"):
+        pas = 144
+        run = False
+    elif (pas == "3"):
+        pas = 72
+        run = False
+    elif (pas == "4"):
+        pas = 36
+        run = False
+    elif (pas == "5"):
+        pas = 6
+        run = False
+    else:
+        print("WRONG RESPONSE !!!")
 
-print(df.temp_1.describe())
+borneInf = int(input("Entrez la borne inférieur: "))
+borneSup = int(input("Entrez la borne supérieur: "))
 
-#df.hist(column='temp_1')
-#df.hist(column='temp_2')
-#df.hist(column='tot_a')
+i = borneInf
+while i < borneSup:
+    ax = plt.gca()
+    df = datas.iloc[i * pas:i * pas + pas, :]
+    df.plot(kind='line', x='date', y='temp_1', ax=ax)
+    df.plot(kind='line', x='date', y='temp_2', ax=ax, color='red')
+    plt.show()
+    i = i + 1
 
-#plt.figure()
-
-#df['temp_1'].plot()
-#df['temp_2'].plot()
+# series = df['date']
+# print(type(series))
+# series.dt.strftime('%Y-%m-%d')
