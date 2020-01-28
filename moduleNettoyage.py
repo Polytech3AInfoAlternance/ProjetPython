d202c027c825fc1e0a5c58421a817ffa0644fedc
quintus42 <46077074+quintus42@users.noreply.github.com>
2020-01-28 15:52:26 +0100

    mise au propre

--- moduleNettoyage.py
+++ moduleNettoyage.py
@@ -6,13 +6,21 @@ import pandas as pd
 #  Définitions des fonctions
 
 def lancer_nettoyage(df, column_name, nb_iter):
-    moyenneGlobale = df[column_name].mean()
-    tab = nettoyage(df, column_name, moyenneGlobale)
-    moyenneGlobale = tab[1]
-    df = tab[0]
-    for k in range(1, nb_iter):
-        tab = nettoyage(df, column_name, moyenneGlobale)
-    return tab[0]
+    moyenneGlobale = get_moyenne_propre(df, column_name)
+    df_temp = df.copy()
+    nettoyage(df, column_name, moyenneGlobale)
+    while not df.equals(df_temp):
+        df_temp = df
+        df = nettoyage(df, column_name, moyenneGlobale)
+
+def lancer_nettoyage_index(df, index, nb_iter):
+    column_name = df.columns[index]
+    moyenneGlobale = get_moyenne_propre(df, column_name)
+    df_temp = df.copy()
+    nettoyage(df, column_name, moyenneGlobale)
+    while not df.equals(df_temp):
+        df_temp = df
+        df = nettoyage(df, column_name, moyenneGlobale)
 
 def nettoyage(df, column_name, moyenne):
     ecart_moyen = 0
@@ -25,37 +33,41 @@ def nettoyage(df, column_name, moyenne):
             ecart = moyenne - ifor_val
         ecart_moyen = ecart_moyen + ecart
     ecart_moyen = ecart_moyen/i
-    compteurMoyenne = 0
     for i, row in df.iterrows():
         val = df.at[i, column_name]
-        if val > 2*ecart_moyen:
-            df.at[i, column_name] = 2*ecart_moyen
+        if val > 1.5*ecart_moyen:
+            df.at[i, column_name] = val%ecart_moyen
+        elif val < -1.5*ecart_moyen:
+            df.at[i, column_name] = -(val%ecart_moyen)
+    return df
+
+def get_moyenne_propre(df, column_name):
+    moyenne = df[column_name].mean()
+    ecart_moyen = 0
+    for i, row in df.iterrows():
+        ifor_val = df.at[i, column_name]
+        ecart = 0
+        if ifor_val >= moyenne:
+            ecart = ifor_val - moyenne
         else:
-            if val < -2*ecart_moyen:
-                df.at[i, column_name] = -2 * ecart_moyen
-            else:
-                compteurMoyenne = compteurMoyenne+df.at[i, column_name]
-    moyenne = compteurMoyenne/i
-    return [df, moyenne]
-
-def remove_outlier(df_in, col_name):
-    q1 = df_in[col_name].quantile(0.25)
-    q3 = df_in[col_name].quantile(0.75)
-    iqr = q3 - q1  # ecart interquartile
-    fence_low = q1 - 1.5 * iqr
-    fence_high = q3 + 1.5 * iqr
-    df_out = df_in.loc[(df_in[col_name] > fence_low) & (df_in[col_name] < fence_high)]
-    df_ab = df_in.loc[(df_in[col_name] < fence_low) & (df_in[col_name] > fence_high)]
-
-    return df_out
-
-# Possiblement utile pour valeur abberante
-# print(df[ np.abs(df.Data - df.Data.mean()) > 1.5*df.Data.std() ])
+            ecart = moyenne - ifor_val
+        ecart_moyen = ecart_moyen + ecart
+    ecart_moyen = ecart_moyen / i
+    compteurMoyenne = 0
+    cpt = 0
+    for i, row in df.iterrows():
+        val = df.at[i, column_name]
+        if val > -2 * ecart_moyen and val < 2 * ecart_moyen:
+            cpt = cpt + 1
+            compteurMoyenne = compteurMoyenne + df.at[i, column_name]
+    moyenne = compteurMoyenne / cpt
+    return moyenne
 
 #  Main
 if __name__ == "__main__":
-    d = {'col1': [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 'col2': [3, -4, 5, 6, -5, 9, 7, 1, 9, -8, 1, 4, -1, 139]}
+    d = {'col1': [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+         'col2': [93, -34, 55, 36, -75, 89, 67, 21, 49, -38, 12, 14, -11112, 1393]}
     df = pd.DataFrame(data=d)
-    lancer_nettoyage(df, 'col2', 6)
+    lancer_nettoyage_index(df, 1, 3)
     print(df)
     print('ok moduleNettoyage')
