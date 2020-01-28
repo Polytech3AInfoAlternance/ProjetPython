14bc7dd87e2472f59d0746901aeeb4f84d326c7d
Aurian <durand.aurian@me.com>
2020-01-28 13:37:41 +0100

    Ajout commentaire fin chargement données

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from moduleChargementDonnees import ChargementManager
 managerDonnees = ChargementManager()
 managerDonnees.ReadJSON('data.json')
 
+print('Chargement des données terminé')
+
 #Exemple d'utilisation afin de récupérer le nom d'un site
 print(managerDonnees.GetSite("0100001797").name)
 
