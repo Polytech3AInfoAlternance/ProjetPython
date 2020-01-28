6102c05f68fb08af0268c7d19448ca766c7a63c2
Aurian <durand.aurian@me.com>
2020-01-28 13:56:43 +0100

    Derniers correctifs

--- main.py
+++ main.py
@@ -6,12 +6,11 @@ managerDonnees.ReadJSON('data.json')
 print('Chargement des données terminé')
 
 #Exemple d'utilisation afin de récupérer le nom d'un site
-print(managerDonnees.GetSite("0100001797").name)
+'''print(managerDonnees.GetSite("0100001797").name)'''
 
 #Passez en parametre 'managerDonnees' afin de ne pas avoir à le créer
 
 # Exemple
-
 '''for site in managerDonnees.GetListNameSite():
     print(managerDonnees.GetSite(site).consoList)
     print(managerDonnees.GetSite(site).tempList)'''
