967d66d784694dd2edbf4ad42a14db6a373c6d0f
leocoulomb <coulombleo@gmail.com>
2020-01-28 16:47:15 +0100

    Merging float data

--- moduleChargementDonneesPackage/csvToDataFrame.py
+++ moduleChargementDonneesPackage/csvToDataFrame.py
@@ -16,7 +16,6 @@ def csv_to_df(path, rows):
     for i in range(0, len(rows)):
         fields.append(rows[i])
     df_result.columns = fields
-
     df_result = df_result.iloc[2:]
     for col in df_result.columns:
         if col != 'Date':
